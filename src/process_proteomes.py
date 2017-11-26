06d328747d8cd988fb8ddba184decc55307319bf
Francesco Beghini <francesco.beghini@unitn.it>
2017-11-26 10:32:35 +0100

    Sync

--- src/process_proteomes.py
+++ src/process_proteomes.py
@@ -23,10 +23,11 @@ import copyreg
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
+from Bio.Phylo.BaseTree import Clade as BClade
 from functools import partial
 
 kingdom_to_process = ['Bacteria','Archaea']
-dbReference = ["EMBL","Ensembl","GeneID","GO","KEGG","KO","Pfam","Refseq","Proteomes"]
+dbReference = ['EMBL','Ensembl','GeneID','GO','KEGG','KO','Pfam','Refseq','Proteomes']
 
 def filt(e):
     return ('end' in e[0]) and ('entry' in e[1].tag)
@@ -59,23 +60,23 @@ def createDataset(filepath):
         utils.error(str(e), exit=True)
 
 def parse_uniprotkb_xml_elem(elem, config, database):
-    tag_to_parse = ["accession","name","dbReference","sequence","organism","sequence"]
+    tag_to_parse = ['accession','name','dbReference','sequence','organism','sequence']
     try:
         d_prot = {}
-        sequence = ""
+        sequence = ''
         org = [x for x in elem.iterchildren('{http://uniprot.org/uniprot}organism')][0]
-        kingdom = [x for x in org.iterchildren("{http://uniprot.org/uniprot}lineage")][0].getchildren()[0].text
+        kingdom = [x for x in org.iterchildren('{http://uniprot.org/uniprot}lineage')][0].getchildren()[0].text
         if kingdom in kingdom_to_process:
             for children in tag_to_parse:
-                tag_children = "{http://uniprot.org/uniprot}"+children
-                if children == "sequence":
+                tag_children = '{http://uniprot.org/uniprot}'+children
+                if children == 'sequence':
                     sequence = [x for x in elem.iterchildren(tag=tag_children)][0].text
-                if children == "name" or children == "accession":
+                if children == 'name' or children == 'accession':
                     d_prot[children] = [x for x in elem.iterchildren(tag=tag_children)][0].text
-                if children == "organism":
+                if children == 'organism':
                     org = [x for x in elem.iterchildren(tag_children)][0]
                     d_prot['tax_id'] = [x.get('id') for x in org.iterchildren(tag='{http://uniprot.org/uniprot}dbReference')][0]
-                if children == "dbReference":
+                if children == 'dbReference':
                     if children not in d_prot:
                         d_prot[children] = {}
                     for ref in elem.iterchildren(tag=tag_children):
@@ -97,7 +98,7 @@ def parse_uniprotkb_xml_elem(elem, config, database):
             for ref in d_prot['dbReference'].keys():
                 str_size = max(map(len,d_prot['dbReference'][ref]))
                 if not protein.__contains__(ref):
-                    reference = protein.create_dataset(ref, (len(d_prot['dbReference'][ref]),), dtype=np.dtype("S{}".format(str_size)), compression="gzip")
+                    reference = protein.create_dataset(ref, (len(d_prot['dbReference'][ref]),), dtype=np.dtype('S{}'.format(str_size)), compression='gzip')
                 
                 protein[ref][:] = d_prot['dbReference'][ref]
 
@@ -126,11 +127,11 @@ def parse_uniprotkb_xml(database, l_input, config):
         if config['verbose']:
             utils.info('Done processing {} file!\n'.format(input))
     if config['verbose']:
-        utils.info("Done\n")
+        utils.info('Done\n')
 
 def create_proteomes(database, config, verbose = False):
     if verbose:
-        utils.info("Starting proteomes processing...\n")
+        utils.info('Starting proteomes processing...\n')
     g_uniprotkb = database['/uniprotkb']
     d_proteomes = {}
     for g_protein in g_uniprotkb.values():
@@ -149,21 +150,21 @@ def create_proteomes(database, config, verbose = False):
     for proteome in d_proteomes:
         str_size = max((map(len,d_proteomes[proteome])))
         if not g_proteomes.__contains__(proteome):
-            g_proteomes.create_dataset(proteome, (len(d_proteomes[proteome]),), dtype=np.dtype("S{}".format(str_size)))
+            g_proteomes.create_dataset(proteome, (len(d_proteomes[proteome]),), dtype=np.dtype('S{}'.format(str_size)))
         g_proteomes[proteome][:] = d_proteomes[proteome]
         
 
         g_proteomes[proteome].attrs['tax_id'] = database['/uniprotkb/{}'.format(d_proteomes[proteome][0])].attrs['tax_id']
         g_proteomes[proteome].attrs['isReference'] = False
         
-    if not os.path.exists("{}/{}".format(config['download_base_dir'],config['relpath_reference_proteomes'])):
+    if not os.path.exists('{}/{}'.format(config['download_base_dir'],config['relpath_reference_proteomes'])):
         utils.error('Required files does not exist! Exiting...', exit=True)
     
     if verbose:
-        utils.info("Starting reference proteomes processing...\n",)
+        utils.info('Starting reference proteomes processing...\n',)
 
     for k in kingdom_to_process:
-        basepath = "{}/{}/{}/*.idmapping.gz".format(config['download_base_dir'],config['relpath_reference_proteomes'], k)
+        basepath = '{}/{}/{}/*.idmapping.gz'.format(config['download_base_dir'],config['relpath_reference_proteomes'], k)
         ls = glob.glob(basepath)
         for protid, taxid in [os.path.split(file)[1].split('.')[0].split('_') for file in ls]:
             protid = protid.encode('utf-8')
@@ -171,37 +172,38 @@ def create_proteomes(database, config, verbose = False):
                 g_proteomes[protid].attrs['tax_id'] = taxid
                 g_proteomes[protid].attrs['isReference'] = True
     if verbose:
-        utils.info("Done\n")
+        utils.info('Done\n')
 
 def parse_uniref_xml_elem(elem, config, database):
     tag_to_parse = ['member', 'representativeMember', 'property'] 
     try:
         d_uniref = {}
-        d_uniref = {}
-        d_uniref["id"] = elem.get('id')
+        d_uniref['id'] = elem.get('id')
         d_uniref['members'] = {}
         for tag in tag_to_parse:
-            tag_children = "{http://uniprot.org/uniref}"+tag
-            if tag == "property":
+            tag_children = '{http://uniprot.org/uniref}'+tag
+            if tag == 'property':
                 for pro in elem.iterchildren(tag_children):
                     if pro.get('type') == 'common taxon ID':
-	    	        d_uniref["common_taxid"] = pro.get('value')
-	    if tag == "representativeMember" or tag == "member":
-	        member = [x for x in elem.iterchildren(tag_children)]
-	        properties = [x.iterchildren('{http://uniprot.org/uniref}property') for y in member for x in y.iterchildren("{http://uniprot.org/uniref}dbReference")]
-	        for p in properties:
-	    	    for pro in p:
-	    	        if pro.get('type') == "UniProtKB accession":
-	    		    accession = pro.get('value')
-	    		    d_uniref['members'][accession] = {}
-	    		    d_uniref['members'][accession]["isRepr"] = True if tag == "representativeMember" else False
-                        if pro.get('type') == "UniRef100 ID":
-	    		    d_uniref['members'][accession]["UniRef100"] = pro.get('value')
-	    		if pro.get('type') == "UniRef90 ID":
-	    		    d_uniref['members'][accession]["UniRef90"] = pro.get('value')
-	    		if pro.get('type') == "UniRef50 ID":
-	    		    d_uniref['members'][accession]["UniRef50"] = pro.get('value')
+                        d_uniref['common_taxid'] = pro.get('value')
+            if tag == 'representativeMember' or tag == 'member':
+                member = [x for x in elem.iterchildren(tag_children)]
+                properties = [x.iterchildren('{http://uniprot.org/uniref}property') for y in member for x in y.iterchildren('{http://uniprot.org/uniref}dbReference')]
+                for p in properties:
+                    for pro in p:
+                        if pro.get('type') == 'UniProtKB accession':
+                            accession = pro.get('value')
+                            d_uniref['members'][accession] = {}
+                            d_uniref['members'][accession]['isRepr'] = True if tag == 'representativeMember' else False
+                        if pro.get('type') == 'UniRef100 ID':
+                            d_uniref['members'][accession]['UniRef100'] = pro.get('value')
+                        if pro.get('type') == 'UniRef90 ID':
+                            d_uniref['members'][accession]['UniRef90'] = pro.get('value')
+                        if pro.get('type') == 'UniRef50 ID':
+                            d_uniref['members'][accession]['UniRef50'] = pro.get('value')
 
+        entry = BClade(clades=[], name = d_uniref['id']
+        
     except Exception as e:
         utils.error(str(e))
         raise
@@ -228,11 +230,11 @@ if __name__ == '__main__':
     config = config['process_proteomes']
 
     with createDataset(config['download_base_dir']+config['relpath_chocophlan_database']) as choco:
-        if not choco.__contains__("/uniprotkb"):
+        if not choco.__contains__('/uniprotkb'):
             choco.create_group('/uniprotkb')
-        parse_uniprotkb_xml(choco, ["data/uniprot/complete/uniprot_sprot.xml", "data/uniprot/complete/uniprot_trembl.xml"], config)
-        #parse_xml(choco,["data/uniprot/complete/uniprot_sprot_example.xml"], config)
-        #parse_uniprotkb_xml(choco,["data/uniprot/complete/uniprot_sprot.xml"], config)
+        parse_uniprotkb_xml(choco, ['data/uniprot/complete/uniprot_sprot.xml', 'data/uniprot/complete/uniprot_trembl.xml'], config)
+        #parse_xml(choco,['data/uniprot/complete/uniprot_sprot_example.xml'], config)
+        #parse_uniprotkb_xml(choco,['data/uniprot/complete/uniprot_sprot.xml'], config)
         create_proteomes(choco,config)
     t1=time.time()
 
